cbe684c40df81d8ba95d7875ce190e4eb1235bbc
Tomirad Trela <tomplus@gmail.com>
2017-10-17 21:39:58 +0200

    Working version

---
+++ Treto/ConvertStrings.php
@@ -0,0 +1,41 @@
+<?php
+/*
+	Interpolacja zmiennych w kodzie źródłowym na zmienne zewnętrzne.
+	v.T3.2017.09.17
+	- poprawa pattern
+*/
+namespace Treto;
+
+class ConvertStrings {
+	private $brackets = [
+		'[]', '[[]]', '{}', '{{}}', '()', '(())', '$$', '$ ', '__', '____'
+	];
+	public function interpolate($string, $data, $bracket = '[]') {
+		if(is_array($data)) {
+			$replace = [];
+			$b = $this->splitBracket($bracket);
+			foreach ($data as $key => $val) {
+				if (!is_array($val) && (!is_object($val) || !method_exists($val, '__toString'))) {
+					$replace[($b->left.$key.$b->right)] = $val;
+				}
+			}
+			return strtr($string, $replace);
+		} else return $string;
+	}
+
+	public function findBrackets($string, $bracket = '[]') {
+		$b = $this->splitBracket($bracket);
+		$pattern = "/[{$b->left}](.*?)[{$b->right}]/";
+		preg_match_all($pattern, $string, $matches);
+		return $matches[1];
+	}
+
+	private function splitBracket($bracket) {
+		$b = new \stdClass;
+		$strlen = strlen($bracket);
+		$b -> left = substr($bracket, 0,$strlen/2);
+		$b -> right = substr($bracket, $strlen/2);
+		return $b;
+	}
+}
+?>
